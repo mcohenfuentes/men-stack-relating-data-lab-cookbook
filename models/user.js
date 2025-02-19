99f65cb1c9e453d277640df9f98929283229af85
Mica CF <mcohenfuentes@gmail.com>
2025-02-19 16:31:37 -0500

    set up to adding passUserToView middlewre

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: true,
+  },
+ 
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +17,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  foods: [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
