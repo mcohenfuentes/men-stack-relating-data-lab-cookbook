be8830a9b9441834e2737a4b9270156d76924b36
Mica CF <mcohenfuentes@gmail.com>
2025-02-21 16:00:43 -0500

    added edit page

--- controllers/foods.js
+++ controllers/foods.js
@@ -74,4 +74,32 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/:foodId/edit', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const food = currentUser.foods.id(req.params.foodId);
+      res.render('foods/edit.ejs', {
+        food: food,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.put('/:foodId', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const food = currentUser.foods.id(req.params.foodId);
+      food.set(req.body);
+      await currentUser.save();
+      res.redirect(
+        `/users/${currentUser._id}/foods/${req.params.foodId}`
+      );
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
 module.exports = router;
