bd0da1433e6ac6acd9288bd4fcd710282a456e9e
Mica CF <mcohenfuentes@gmail.com>
2025-02-21 15:25:13 -0500

    built delete route

--- controllers/foods.js
+++ controllers/foods.js
@@ -39,4 +39,39 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/:foodId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Find the application by the applicationId supplied from req.params
+      const food = currentUser.foods.id(req.params.foodId);
+      // Render the show view, passing the application data in the context object
+      res.render('foods/show.ejs', {
+        food: food,
+      });
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.delete('/:foodId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Use the Mongoose .deleteOne() method to delete
+      // an application using the id supplied from req.params
+      currentUser.foods.id(req.params.foodId).deleteOne();
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
 module.exports = router;
